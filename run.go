78bd61b608db20ac1d3a8a5e93279997424ae466
tamal <tamal@appscode.com>
2017-07-18 19:02:59 -0700

    Update tags to 0.5.0

--- run.go
+++ run.go
@@ -33,7 +33,7 @@ var (
 	kubeconfigPath    string
 	governingService  string = "kubedb"
 	exporterTag       string = stringz.Val(Version, "canary")
-	esOperatorTag     string = "0.4.0"
+	esOperatorTag     string = "0.5.0"
 	elasticDumpTag    string = "2.4.2"
 	address           string = ":8080"
 	operatorNamespace string = namespace()
