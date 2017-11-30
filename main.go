7d95782c8c627862424abac493b42558db6b525f
the-redback <maruf@appscode.com>
2017-11-30 14:16:33 +0600

    Add Redis Exporter

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	logs.InitLogs()
 	defer logs.FlushLogs()
-	if err := cmds.NewRootCmd("mongoExporter").Execute(); err != nil {
+	if err := cmds.NewRootCmd(Version).Execute(); err != nil {
 		log.Fatal(err)
 	}
 }
