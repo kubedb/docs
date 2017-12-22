6d4498b0f510f44208a3ebc0f42a54edef90df2d
Tamal Saha <tamal@appscode.com>
2017-12-22 09:58:25 -0500

    Simplify operator docker file

--- hack/docker/setup.sh
+++ hack/docker/setup.sh
@@ -44,14 +44,12 @@ build_docker() {
 FROM alpine
 
 RUN set -x \
-    && apk update \
-    && apk add ca-certificates \
-    && rm -rf /var/cache/apk/*
+  && apk add --update --no-cache ca-certificates
 
-COPY operator /operator
+COPY operator /usr/bin/operator
 
 USER nobody:nobody
-ENTRYPOINT ["/operator"]
+ENTRYPOINT ["operator"]
 EOL
     local cmd="docker build -t kubedb/$IMG:$TAG ."
     echo $cmd; $cmd
