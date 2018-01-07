9a0b7c92dac8270327eba83250788f8e372cf4e3
tamal <tamal@appscode.com>
2018-01-06 17:01:29 -0800

    Add release script

---
+++ hack/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -xeou pipefail
+
+GOPATH=$(go env GOPATH)
+REPO_ROOT="$GOPATH/src/github.com/kubedb/operator"
+
+export APPSCODE_ENV=prod
+
+pushd $REPO_ROOT
+
+rm -rf dist
+
+./hack/docker/setup.sh
+./hack/docker/setup.sh release
+
+rm dist/.tag
+
+popd
