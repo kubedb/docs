53464a0e83f3a55c1fe5a04c04024bf29d4fe4dc
tahsinrahman <tahsynx@gmail.com>
2018-06-11 13:45:09 +0600

    change vm size from 2vcpu 8gb to 1vcpu 4gb

--- hack/concourse/cluster.sh
+++ hack/concourse/cluster.sh
@@ -163,7 +163,7 @@ function prepare_aks {
 function prepare_acs {
     azure_common
     set +x
-    az acs create --orchestrator-type kubernetes --orchestrator-version $K8S_VERSION --resource-group $NAME --name $NAME --agent-count 1 --service-principal $APP_ID --client-secret $PASSWORD --generate-ssh-keys &> /dev/null
+    az acs create --orchestrator-type kubernetes --orchestrator-version $K8S_VERSION --resource-group $NAME --name $NAME --master-vm-size $NODE --agent-vm-size $NODE --agent-count 1 --service-principal $APP_ID --client-secret $PASSWORD --generate-ssh-keys &> /dev/null
     set -x
     az acs kubernetes get-credentials --resource-group $NAME --name $NAME
 }
