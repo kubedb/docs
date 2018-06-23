b24edf3eac2f8be8092c1728398cad5c4b5713c0
Md. Tahsin Rahman <tahsynx@gmail.com>
2018-06-24 00:34:17 +0600

    Concourse test on kubespray (#174)

--- hack/concourse/cluster.sh
+++ hack/concourse/cluster.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
 
 set -eoux pipefail
 
+export ClusterProvider=${ClusterProvider:-digitalocean}
 export CredProvider=${CredProvider:-DigitalOcean}
 export ZONE=${ZONE:-nyc1}
 export NODE=${NODE:-4gb}
-export K8S_VERSION=${K8S_VERSION:-1.10.0}
+export K8S_VERSION=${K8S_VERSION:-v1.10.0}
+export StorageClass="standard"
 
 # name of the cluster
 pushd operator
-export NAME=operator-$(git rev-parse --short HEAD)
+export NAME
+NAME=operator-$(git rev-parse --short HEAD)
 popd
 
 function cleanup {
@@ -35,33 +39,36 @@ function cleanup {
     kubectl describe nodes
 
     # delete cluster on exit
-    if [ "${ClusterProvider}" = "aws" ]; then
-        kops delete cluster --name ${NAME} --yes
-    elif [[ "${ClusterProvider}" = "aks" || "${ClusterProvider}" = "acs" ]]; then
-        az group delete --name $NAME --yes --no-wait
+    if [ "$ClusterProvider" = "aws" ]; then
+        kops delete cluster --name "$NAME" --yes
+    elif [[ "$ClusterProvider" = "aks" || "$ClusterProvider" = "acs" ]]; then
+        az group delete --name "$NAME" --yes --no-wait
+    elif [ "$ClusterProvider" = "kubespray" ]; then
+        packet admin delete-sshkey --key-id "$SSH_KEY_ID" --key "$PACKET_API_TOKEN"
+        packet baremetal delete-device --device-id "$DEVICE_ID" --key "$PACKET_API_TOKEN"
     else
         pharmer get cluster
-        pharmer delete cluster ${NAME}
+        pharmer delete cluster "$NAME"
         pharmer get cluster
         sleep 300
-        pharmer apply ${NAME}
+        pharmer apply "$NAME"
         pharmer get cluster
     fi
 
     # delete docker image on exit
     curl -LO https://raw.githubusercontent.com/appscodelabs/libbuild/master/docker.py
     chmod +x docker.py
-    ./docker.py del_tag kubedbci operator ${CUSTOM_OPERATOR_TAG}
+    ./docker.py del_tag kubedbci operator "$CUSTOM_OPERATOR_TAG"
 }
 trap cleanup EXIT
 
 function pharmer_common {
     # create cluster using pharmer
-    pharmer create credential --from-file=creds/${ClusterProvider}.json --provider=${CredProvider} cred
-    pharmer create cluster ${NAME} --provider=${ClusterProvider} --zone=${ZONE} --nodes=${NODE}=1 --credential-uid=cred --v=10 --kubernetes-version=${K8S_VERSION}
-    pharmer apply ${NAME} || true
-    pharmer apply ${NAME}
-    pharmer use cluster ${NAME}
+    pharmer create credential --from-file=creds/"$ClusterProvider".json --provider="$CredProvider" cred
+    pharmer create cluster "$NAME" --provider="$ClusterProvider" --zone="$ZONE" --nodes="$NODE"=1 --credential-uid=cred --v=10 --kubernetes-version="$K8S_VERSION"
+    pharmer apply "$NAME" || true
+    pharmer apply "$NAME"
+    pharmer use cluster "$NAME"
     sleep 120
 }
 
@@ -75,7 +82,7 @@ function prepare_gke {
     source /tmp/google-cloud-sdk/path.bash.inc
     popd
     gcloud auth activate-service-account --key-file creds/gke.json
-    gcloud container clusters get-credentials ${NAME} --zone ${ZONE} --project k8s-qa
+    gcloud container clusters get-credentials "$NAME" --zone "$ZONE" --project k8s-qa
     kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=k8s-qa@k8s-qa.iam.gserviceaccount.com
 
     # wait for cluster to be ready
@@ -84,7 +91,7 @@ function prepare_gke {
 
 function prepare_aws {
     # install kops
-    curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+    curl -Lo kops https://github.com/kubernetes/kops/releases/download/"$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)"/kops-linux-amd64
     chmod +x ./kops
     mv ./kops /usr/local/bin/
 
@@ -101,7 +108,8 @@ function prepare_aws {
 
     # name of the cluster
     pushd operator
-    export NAME=operator-$(git rev-parse --short HEAD).k8s.local
+    export NAME
+    NAME=operator-$(git rev-parse --short HEAD).k8s.local
     popd
 
     # use s3 bucket for cluster state storage
@@ -114,10 +122,10 @@ function prepare_aws {
     ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa
 
     # generate cluster configuration
-    kops create cluster --zones us-east-1a  --node-count 1 ${NAME}
+    kops create cluster --zones us-east-1a  --node-count 1 "$NAME"
 
     # build cluster
-    kops update cluster ${NAME} --yes
+    kops update cluster "$NAME" --yes
 
     # wait for cluster to be ready
     end=$((SECONDS+900))
@@ -146,29 +154,107 @@ function azure_common {
 
     # login with service principal
     set +x
-    az login --service-principal --username $APP_ID --password $PASSWORD --tenant $TENANT_ID &> /dev/null
-    az group create --name $NAME --location $ZONE
+    az login --service-principal --username "$APP_ID" --password "$PASSWORD" --tenant "$TENANT_ID" &> /dev/null
+    az group create --name "$NAME" --location "$ZONE"
     set -x
 }
 
 function prepare_aks {
     azure_common
     set +x
-    az aks create --resource-group $NAME --name $NAME --service-principal $APP_ID --client-secret $PASSWORD --generate-ssh-keys --node-vm-size $NODE --node-count 1 --kubernetes-version $K8S_VERSION &> /dev/null
+    az aks create --resource-group "$NAME" --name "$NAME" --service-principal "$APP_ID" --client-secret "$PASSWORD" --generate-ssh-keys --node-vm-size "$NODE" --node-count 1 --kubernetes-version "$K8S_VERSION" &> /dev/null
     set -x
-    az aks get-credentials --resource-group $NAME --name $NAME
+    az aks get-credentials --resource-group "$NAME" --name "$NAME"
 
 }
 
 function prepare_acs {
     azure_common
     set +x
-    az acs create --orchestrator-type kubernetes --orchestrator-version $K8S_VERSION --resource-group $NAME --name $NAME --master-vm-size $NODE --agent-vm-size $NODE --agent-count 1 --service-principal $APP_ID --client-secret $PASSWORD --generate-ssh-keys &> /dev/null
+    az acs create --orchestrator-type kubernetes --orchestrator-version "$K8S_VERSION" --resource-group "$NAME" --name "$NAME" --master-vm-size "$NODE" --agent-vm-size "$NODE" --agent-count 1 --service-principal "$APP_ID" --client-secret "$PASSWORD" --generate-ssh-keys &> /dev/null
     set -x
-    az acs kubernetes get-credentials --resource-group $NAME --name $NAME
+    az acs kubernetes get-credentials --resource-group "$NAME" --name "$NAME"
 }
 
-export StorageClass="standard"
+function prepare_kubespray {
+    apt-get update
+    apt-get install -y jq
+
+    ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa
+    go get -u github.com/ebsarr/packet
+
+    export PACKET_API_TOKEN=${PACKET_API_TOKEN:-}
+    export PACKET_PROJECT_ID=${PACKET_PROJECT_ID:-}
+
+    packet admin create-sshkey -f /root/.ssh/id_rsa.pub --label "$NAME" --key="$PACKET_API_TOKEN" > ssh_key.js
+    export SSH_KEY_ID
+    SSH_KEY_ID=$(jq -r .id ssh_key.js)
+
+    packet baremetal create-device --facility ams1 --hostname "$NAME" --os-type ubuntu_16_04 --project-id "$PACKET_PROJECT_ID"  --key="$PACKET_API_TOKEN" > js.json
+
+    export DEVICE_ID
+    DEVICE_ID=$(jq -r .id js.json)
+
+    export PUBLIC_IP
+    PUBLIC_IP=$(jq -r .ip_addresses[0].address js.json)
+
+    ssh -o "StrictHostKeyChecking no" root@"$PUBLIC_IP" swapoff -a
+
+    apt-get install -y ansible
+
+    git clone https://github.com/kubernetes-incubator/kubespray.git
+    pushd kubespray
+    git checkout -b tags/v2.5.0
+
+    pip install -r requirements.txt
+    cp -rfp inventory/sample inventory/mycluster
+
+    cat > inventory/mycluster/hosts.ini <<EOF
+[all]
+$NAME ansible_host=$PUBLIC_IP ip=$PUBLIC_IP
+
+[kube-master]
+$NAME
+
+[kube-node]
+$NAME
+
+[etcd]
+$NAME
+
+[k8s-cluster:children]
+kube-node
+kube-master
+
+[calico-rr]
+
+
+[vault]
+$NAME
+EOF
+
+    ansible-playbook -u root -i inventory/mycluster/hosts.ini  cluster.yml -b -v
+    popd
+
+    mkdir -p /root/.kube
+    scp root@"$PUBLIC_IP":/root/.kube/config /root/.kube
+
+    # rook
+    git clone https://github.com/rook/rook
+    pushd rook/cluster/examples/kubernetes/ceph/
+
+    sed -i '212s/^/        - name: FLEXVOLUME_DIR_PATH\n/' operator.yaml
+    sed -i '213s/^/          value: "\/var\/lib\/kubelet\/volume-plugins"\n/' operator.yaml
+
+    kubectl create -f operator.yaml
+    kubectl create -f cluster.yaml
+    sleep 120
+    kubectl create -f storageclass.yaml
+    export StorageClass="rook-ceph-block"
+
+    popd
+}
+
 
 # prepare cluster
 if [ "${ClusterProvider}" = "gke" ]; then
@@ -179,6 +265,8 @@ elif [ "${ClusterProvider}" = "aks" ]; then
     prepare_aks
 elif [ "${ClusterProvider}" = "acs" ]; then
     prepare_acs
+elif [ "${ClusterProvider}" = "kubespray" ]; then
+    prepare_kubespray
 elif [ "${ClusterProvider}" = "digitalocean" ]; then
     pharmer_common
 
--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -1,26 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC1091
 
 set -eoux pipefail
 
 GOPATH=$(go env GOPATH)
 REPO_ROOT="$GOPATH/src/github.com/kubedb/operator"
+export ClusterProvider=${ClusterProvider:-digitalocean}
+export StorageClass=${StorageClass:-standard}
 
 # copy operator to $GOPATH
-mkdir -p $GOPATH/src/github.com/kubedb
-cp -r operator $GOPATH/src/github.com/kubedb
+mkdir -p "$GOPATH"/src/github.com/kubedb
+cp -r operator "$GOPATH"/src/github.com/kubedb
 
 # install all the dependencies and prepeare cluster
 source "$REPO_ROOT/hack/concourse/dependencies.sh"
 source "$REPO_ROOT/hack/concourse/cluster.sh"
 
 # build and push operator docker-image
-pushd $GOPATH/src/github.com/kubedb/operator
+pushd "$GOPATH"/src/github.com/kubedb/operator
 
 # changed name of branch
 # this is necessary because operator image tag is based on branch name
 # for parallel tests, if two test build image of same tag, it'll create problem
 # one test may finish early and delete image while other is using it
-git branch -m $(git rev-parse --abbrev-ref HEAD)-${ClusterProvider}
+git branch -m "$(git rev-parse --abbrev-ref HEAD)-$ClusterProvider"
 
 ./hack/builddeps.sh
 export APPSCODE_ENV=dev
@@ -30,33 +33,12 @@ export DOCKER_REGISTRY=kubedbci
 ./hack/docker/setup.sh push
 popd
 
-export CRED_DIR=$(pwd)/creds/gcs.json
+cp creds/gcs.json /gcs.json
 
 # create config/.env file that have all necessary creds
-cat > /tmp/.env <<EOF
-AWS_ACCESS_KEY_ID=$AWS_KEY_ID
-AWS_SECRET_ACCESS_KEY=$AWS_SECRET
+cp creds/.env /tmp/.env
 
-GOOGLE_PROJECT_ID=$GCE_PROJECT_ID
-GOOGLE_APPLICATION_CREDENTIALS=$CRED_DIR
-
-AZURE_ACCOUNT_NAME=$AZURE_ACCOUNT_NAME
-AZURE_ACCOUNT_KEY=$AZURE_ACCOUNT_KEY
-
-OS_AUTH_URL=$OS_AUTH_URL
-OS_TENANT_ID=$OS_TENANT_ID
-OS_TENANT_NAME=$OS_TENANT_NAME
-OS_USERNAME=$OS_USERNAME
-OS_PASSWORD=$OS_PASSWORD
-OS_REGION_NAME=$OS_REGION_NAME
-
-S3_BUCKET_NAME=$S3_BUCKET_NAME
-GCS_BUCKET_NAME=$GCS_BUCKET_NAME
-AZURE_CONTAINER_NAME=$AZURE_CONTAINER_NAME
-SWIFT_CONTAINER_NAME=$SWIFT_CONTAINER_NAME
-EOF
-
-pushd $GOPATH/src/github.com/kubedb
+pushd "$GOPATH"/src/github.com/kubedb
 
 # deploy operator
 pushd operator
@@ -69,11 +51,13 @@ EXIT_CODE=0
 echo "======================TESTING REDIS=============================="
 git clone https://github.com/kubedb/redis
 pushd redis
-if ! (./hack/make.py test e2e --v=1 --storageclass=$StorageClass --selfhosted-operator=true); then
+if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
     EXIT_CODE=1
 fi
 popd
+sleep 120
 
+kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
 echo ""
 echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
@@ -86,11 +70,13 @@ kubectl describe nodes || true
 echo "======================TESTING MEMCACHED=============================="
 git clone https://github.com/kubedb/memcached
 pushd memcached
-if ! (./hack/make.py test e2e --v=1 --selfhosted-operator=true); then
+if ! (./hack/make.py test e2e --v=1 --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
     EXIT_CODE=1
 fi
 popd
+sleep 120
 
+kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
 echo ""
 echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
@@ -104,11 +90,13 @@ echo "======================TESTING ELASTICSEARCH============================="
 git clone https://github.com/kubedb/elasticsearch
 pushd elasticsearch
 cp /tmp/.env hack/config/.env
-if ! (./hack/make.py test e2e --v=1 --storageclass=$StorageClass --selfhosted-operator=true); then
+if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
     EXIT_CODE=1
 fi
 popd
+sleep 120
 
+kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
 echo ""
 echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
@@ -127,11 +115,13 @@ cp /tmp/.env hack/config/.env
 ./hack/docker/postgres/9.6/make.sh
 ./hack/docker/postgres/10.2/make.sh build
 ./hack/docker/postgres/10.2/make.sh push
-if ! (./hack/make.py test e2e --v=1 --storageclass=$StorageClass --selfhosted-operator=true); then
+if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
     EXIT_CODE=1
 fi
 popd
+sleep 120
 
+kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
 echo ""
 echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
@@ -145,11 +135,13 @@ echo "======================TESTING MONGODB=============================="
 git clone https://github.com/kubedb/mongodb
 pushd mongodb
 cp /tmp/.env hack/config/.env
-if ! (./hack/make.py test e2e --v=1 --storageclass=$StorageClass --selfhosted-operator=true); then
+if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
     EXIT_CODE=1
 fi
 popd
+sleep 120
 
+kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
 echo ""
 echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
@@ -163,11 +155,12 @@ echo "======================TESTING MYSQL=============================="
 git clone https://github.com/kubedb/mysql
 pushd mysql
 cp /tmp/.env hack/config/.env
-if ! (./hack/make.py test e2e --v=1 --storageclass=$StorageClass --selfhosted-operator=true); then
+if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
     EXIT_CODE=1
 fi
 popd
 
+kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
 echo ""
 echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
