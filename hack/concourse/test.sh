a06f920bb52383d02a5818adeb0f2ac3fb3b7210
Md. Tahsin Rahman <tahsynx@gmail.com>
2018-08-17 11:20:22 +0600

    Add support for running tests on cncf cluster (#181)
    * remove concourse scripts from repo root
    * update pipeline to support for cncf cluster
    * trigger tests of user is from kubedb org

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -1,42 +1,35 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC1090,SC1091
 
 set -eoux pipefail
 
-GOPATH=$(go env GOPATH)
-REPO_ROOT="$GOPATH/src/github.com/kubedb/operator"
-export ClusterProvider=${ClusterProvider:-digitalocean}
-export StorageClass=${StorageClass:-standard}
+ORG_NAME=kubedb
+REPO_NAME=operator
+OPERATOR_NAME=operator
+APP_LABEL=kubedb #required for `kubectl describe deploy -n kube-system -l app=$APP_LABEL`
 
-# copy operator to $GOPATH
-mkdir -p "$GOPATH"/src/github.com/kubedb
-cp -r operator "$GOPATH"/src/github.com/kubedb
-
-# install all the dependencies and prepeare cluster
-source "$REPO_ROOT/hack/concourse/dependencies.sh"
-source "$REPO_ROOT/hack/concourse/cluster.sh"
-
-# build and push operator docker-image
-pushd "$GOPATH"/src/github.com/kubedb/operator
-
-# changed name of branch
-# this is necessary because operator image tag is based on branch name
-# for parallel tests, if two test build image of same tag, it'll create problem
-# one test may finish early and delete image while other is using it
-git branch -m "$(git rev-parse --abbrev-ref HEAD)-$ClusterProvider"
-
-./hack/builddeps.sh
 export APPSCODE_ENV=dev
 export DOCKER_REGISTRY=kubedbci
 
+# get concourse-common
+pushd $REPO_NAME
+git status # required, otherwise you'll get error `Working tree has modifications.  Cannot add.`. why?
+git subtree pull --prefix hack/libbuild https://github.com/appscodelabs/libbuild.git master --squash -m 'concourse'
+popd
+
+source $REPO_NAME/hack/libbuild/concourse/init.sh
+
+cp creds/gcs.json /gcs.json
+cp creds/.env /tmp/.env
+
+pushd "$GOPATH"/src/github.com/$ORG_NAME/$REPO_NAME
+./hack/builddeps.sh
 ./hack/docker/setup.sh build
 ./hack/docker/setup.sh push
 popd
 
-cp creds/gcs.json /gcs.json
-
 # create config/.env file that have all necessary creds
 cp creds/.env /tmp/.env
+cp creds/gcs.json /gcs.json
 
 pushd "$GOPATH"/src/github.com/kubedb
 
@@ -48,65 +41,62 @@ popd
 EXIT_CODE=0
 
 # test redis
-echo "======================TESTING REDIS=============================="
+cowsay -f tux "testing redis"
 git clone https://github.com/kubedb/redis
 pushd redis
-if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
+if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=2); then
   EXIT_CODE=1
 fi
 popd
 sleep 120
 
+cowsay -f tux "describe pods"
 kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
-echo ""
-echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
-echo ""
+
+cowsay -f tux "describe nodes"
 kubectl get nodes || true
-echo ""
 kubectl describe nodes || true
 
 # test memcached
-echo "======================TESTING MEMCACHED=============================="
+cowsay -f tux "testing memcached"
 git clone https://github.com/kubedb/memcached
 pushd memcached
-if ! (./hack/make.py test e2e --v=1 --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
+if ! (./hack/make.py test e2e --v=1 --selfhosted-operator=true --ginkgo.flakeAttempts=2); then
   EXIT_CODE=1
 fi
 popd
 sleep 120
 
+cowsay -f tux "describe pods"
 kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
-echo ""
-echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
-echo ""
+
+cowsay -f tux "describe nodes"
 kubectl get nodes || true
-echo ""
 kubectl describe nodes || true
 
 # test elasticsearch
-echo "======================TESTING ELASTICSEARCH============================="
+cowsay -f tux "testing elasticsearch"
 git clone https://github.com/kubedb/elasticsearch
 pushd elasticsearch
 cp /tmp/.env hack/config/.env
-if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
+if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=2); then
   EXIT_CODE=1
 fi
 popd
 sleep 120
 
+cowsay -f tux "describe pods"
 kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
-echo ""
-echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
-echo ""
+
+cowsay -f tux "describe nodes"
 kubectl get nodes || true
-echo ""
 kubectl describe nodes || true
 
 # test postgres
-echo "======================TESTING POSTGRES=============================="
+cowsay -f tux "testing postgres"
 git clone https://github.com/kubedb/postgres
 pushd postgres
 cp /tmp/.env hack/config/.env
@@ -115,58 +105,55 @@ cp /tmp/.env hack/config/.env
 ./hack/docker/postgres/9.6/make.sh
 ./hack/docker/postgres/10.2/make.sh build
 ./hack/docker/postgres/10.2/make.sh push
-if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
+if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=2); then
   EXIT_CODE=1
 fi
 popd
 sleep 120
 
+cowsay -f tux "describe pods"
 kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
-echo ""
-echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
-echo ""
+
+cowsay -f tux "describe nodes"
 kubectl get nodes || true
-echo ""
 kubectl describe nodes || true
 
 # test mongodb
-echo "======================TESTING MONGODB=============================="
+cowsay -f tux "testing mongodb"
 git clone https://github.com/kubedb/mongodb
 pushd mongodb
 cp /tmp/.env hack/config/.env
-if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
+if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=2); then
   EXIT_CODE=1
 fi
 popd
 sleep 120
 
+cowsay -f tux "describe pods"
 kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
-echo ""
-echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
-echo ""
+
+cowsay -f tux "describe nodes"
 kubectl get nodes || true
-echo ""
 kubectl describe nodes || true
 
 # test mysql
-echo "======================TESTING MYSQL=============================="
+cowsay -f tux "testing mysql"
 git clone https://github.com/kubedb/mysql
 pushd mysql
 cp /tmp/.env hack/config/.env
-if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=3); then
+if ! (./hack/make.py test e2e --v=1 --storageclass="$StorageClass" --selfhosted-operator=true --ginkgo.flakeAttempts=2); then
   EXIT_CODE=1
 fi
 popd
 
+cowsay -f tux "describe pods"
 kubectl get pods --all-namespaces
 kubectl describe pods -n kube-system -l app=kubedb || true
-echo ""
-echo "::::::::::::::::::::::::::: Describe Nodes :::::::::::::::::::::::::::"
-echo ""
+
+cowsay -f tux "describe nodes"
 kubectl get nodes || true
-echo ""
 kubectl describe nodes || true
 
 popd
