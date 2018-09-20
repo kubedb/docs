eedef6850280f49f5c20d0d427f211fe3c87538b
Tamal Saha <tamal@appscode.com>
2018-09-20 15:21:14 -0700

    Use --pull flag with docker build (#20) xref: https://github.com/appscodelabs/issues/issues/90
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/docker/setup.sh
+++ hack/docker/setup.sh
@@ -42,7 +42,7 @@ build_docker() {
   chmod 755 operator
 
   cat >Dockerfile <<EOL
-FROM alpine
+FROM alpine:3.8
 
 RUN set -x \
   && apk add --update --no-cache ca-certificates openssl openjdk8-jre-base
@@ -52,7 +52,7 @@ COPY operator /usr/bin/operator
 USER nobody:nobody
 ENTRYPOINT ["operator"]
 EOL
-  local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
+  local cmd="docker build --pull -t $DOCKER_REGISTRY/$IMG:$TAG ."
   echo $cmd; $cmd
 
   rm operator Dockerfile
