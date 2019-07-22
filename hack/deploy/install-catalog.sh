2cfbbb1551f2c5b1a9b98f8a8c52fb23587be5de
Tamal Saha <tamal@appscode.com>
2019-07-21 18:22:51 -0700

    Fix dev deployment script for operator (part-2) (#231)
    Fix https://github.com/kubedb/project/issues/597
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/deploy/install-catalog.sh
+++ hack/deploy/install-catalog.sh
@@ -30,12 +30,67 @@ kubectl config current-context || {
 }
 echo ""
 
+OS=""
+ARCH=""
+DOWNLOAD_URL=""
+DOWNLOAD_DIR=""
+TEMP_DIRS=()
+ONESSL=""
+
+GOPATH=$(go env GOPATH)
+REPO_ROOT=`git rev-parse --show-toplevel`
+INSTALLER_ROOT="$GOPATH/src/kubedb.dev/installer"
+
+pushd $REPO_ROOT
+
 # http://redsymbol.net/articles/bash-exit-traps/
 function cleanup() {
-  rm -rf $ONESSL ca.crt ca.key server.crt server.key
+  rm -rf ca.crt ca.key server.crt server.key
+  # remove temporary directories
+  for dir in "${TEMP_DIRS[@]}"; do
+    rm -rf "${dir}"
+  done
 }
 trap cleanup EXIT
 
+# detect operating system
+# ref: https://raw.githubusercontent.com/helm/helm/master/scripts/get
+function detectOS() {
+  OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
+
+  case "$OS" in
+    # Minimalist GNU for Windows
+    cygwin* | mingw* | msys*) OS='windows';;
+  esac
+}
+
+# detect machine architecture
+function detectArch() {
+  ARCH=$(uname -m)
+  case $ARCH in
+    armv7*) ARCH="arm";;
+    aarch64) ARCH="arm64";;
+    x86) ARCH="386";;
+    x86_64) ARCH="amd64";;
+    i686) ARCH="386";;
+    i386) ARCH="386";;
+  esac
+}
+
+detectOS
+detectArch
+
+# download file pointed by DOWNLOAD_URL variable
+# store download file to the directory pointed by DOWNLOAD_DIR variable
+# you have to sent the output file name as argument. i.e. downloadFile myfile.tar.gz
+function downloadFile() {
+  if curl --output /dev/null --silent --head --fail "$DOWNLOAD_URL"; then
+    curl -fsSL ${DOWNLOAD_URL} -o $DOWNLOAD_DIR/$1
+  else
+    echo "File does not exist"
+    exit 1
+  fi
+}
 
 onessl_found() {
   # https://stackoverflow.com/a/677212/244009
@@ -51,36 +106,27 @@ onessl_found() {
   return 1
 }
 
+# download onessl if it does not exist
 onessl_found || {
   echo "Downloading onessl ..."
-  # ref: https://stackoverflow.com/a/27776822/244009
-  case "$(uname -s)" in
-    Darwin)
-      curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.10.0/onessl-darwin-amd64
-      chmod +x onessl
-      export ONESSL=./onessl
-      ;;
 
-    Linux)
-      curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.10.0/onessl-linux-amd64
-      chmod +x onessl
-      export ONESSL=./onessl
-      ;;
-
-    CYGWIN* | MINGW* | MSYS*)
-      curl -fsSL -o onessl.exe https://github.com/kubepack/onessl/releases/download/0.10.0/onessl-windows-amd64.exe
-      chmod +x onessl.exe
-      export ONESSL=./onessl.exe
-      ;;
-    *)
-      echo 'other OS'
-      ;;
+  ARTIFACT="https://github.com/kubepack/onessl/releases/download/0.12.0"
+  ONESSL_BIN=onessl-${OS}-${ARCH}
+  case "$OS" in
+    cygwin* | mingw* | msys*)
+      ONESSL_BIN=${ONESSL_BIN}.exe
+    ;;
   esac
-}
 
-GOPATH=$(go env GOPATH)
-REPO_ROOT=`git rev-parse --show-toplevel`
-INSTALLER_ROOT="$GOPATH/src/github.com/kubedb/installer"
+  DOWNLOAD_URL=${ARTIFACT}/${ONESSL_BIN}
+  DOWNLOAD_DIR="$(mktemp -dt onessl-XXXXXX)"
+  TEMP_DIRS+=($DOWNLOAD_DIR) # store DOWNLOAD_DIR to cleanup later
+
+  downloadFile $ONESSL_BIN # downloaded file name will be saved as the value of ONESSL_BIN variable
+
+  export ONESSL=${DOWNLOAD_DIR}/${ONESSL_BIN}
+  chmod +x $ONESSL
+}
 
 source "$REPO_ROOT/hack/deploy/settings"
 
