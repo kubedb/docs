860b45a3ef7bbebf7d29c87756a73f81c2ed76b6
Tamal Saha <tamal@appscode.com>
2020-06-22 09:15:47 -0700

    Add workflow to update docs
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/gendocs/main.go
+++ hack/gendocs/main.go
@@ -13,32 +13,118 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"text/template"
 
 	"kubedb.dev/operator/pkg/cmds"
 
 	"github.com/appscode/go/runtime"
 	"github.com/spf13/cobra/doc"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 )
+
+var (
+	tplFrontMatter = template.Must(template.New("index").Parse(`---
+title: Reference | KubeDB Operator
+description: KubeDB Operator Reference
+menu:
+  docs_{{ "{{ .version }}" }}:
+    identifier: reference-operator
+    name: KubeDB Operator
+    weight: 10
+    parent: reference
+menu_name: docs_{{ "{{ .version }}" }}
+---
+`))
+
+	_ = template.Must(tplFrontMatter.New("cmd").Parse(`---
+title: {{ .Name }}
+menu:
+  docs_{{ "{{ .version }}" }}:
+    identifier: {{ .ID }}
+    name: {{ .Name }}
+    parent: reference-operator
+{{- if .RootCmd }}
+    weight: 0
+{{ end }}
+menu_name: docs_{{ "{{ .version }}" }}
+section_menu_id: reference
+{{- if .RootCmd }}
+url: /docs/{{ "{{ .version }}" }}/reference/operator/
+aliases:
+- /docs/{{ "{{ .version }}" }}/reference/operator/{{ .ID }}/
+{{- end }}
+---
+`))
+)
+
+func docsDir() string {
+	if dir, ok := os.LookupEnv("DOCS_ROOT"); ok {
+		return dir
+	}
+	return runtime.GOPath() + "/src/kubedb.dev/docs"
+}
 
 // ref: https://github.com/spf13/cobra/blob/master/doc/md_docs.md
 func main() {
 	rootCmd := cmds.NewRootCmd("")
-	dir := runtime.GOPath() + "/src/kubedb.dev/operator/docs/reference"
+	dir := filepath.Join(docsDir(), "docs", "reference", "operator")
 	fmt.Printf("Generating cli markdown tree in: %v\n", dir)
 	err := os.RemoveAll(dir)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 	err = os.MkdirAll(dir, 0755)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
-	utilruntime.Must(doc.GenMarkdownTree(rootCmd, dir))
+
+	filePrepender := func(filename string) string {
+		name := filepath.Base(filename)
+		base := strings.TrimSuffix(name, path.Ext(name))
+		data := struct {
+			ID      string
+			Name    string
+			RootCmd bool
+		}{
+			strings.Replace(base, "_", "-", -1),
+			strings.Title(strings.Replace(base, "_", " ", -1)),
+			!strings.ContainsRune(base, '_'),
+		}
+		var buf bytes.Buffer
+		if err := tplFrontMatter.ExecuteTemplate(&buf, "cmd", data); err != nil {
+			log.Fatalln(err)
+		}
+		return buf.String()
+	}
+
+	linkHandler := func(name string) string {
+		return "/docs/reference/operator/" + name
+	}
+	err = doc.GenMarkdownTreeCustom(rootCmd, dir, filePrepender, linkHandler)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	index := filepath.Join(dir, "_index.md")
+	f, err := os.OpenFile(index, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tplFrontMatter.ExecuteTemplate(f, "index", struct{}{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatalln(err)
+	}
 }
